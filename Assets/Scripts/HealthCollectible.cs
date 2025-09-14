b3c3a50daa410c42469b7423139fe3472da87b4b
Nguyễn Văn Hoài <husstress2002@gmail.com>
2025-09-14 15:21:26 +0700

    Add Health Collectibles

---
+++ Assets/Scripts/HealthCollectible.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthCollectible : MonoBehaviour
+{
+    public int healthAmount = 1;
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        PlayerController controller = other.GetComponent<PlayerController>();
+        if (controller != null)
+        {
+            controller.ChangeHealth(healthAmount);
+            Destroy(gameObject);
+        }
+    }
+}
