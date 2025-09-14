582dc1f8c9fbd2cc688ff28d915f5bcfb954b076
Nguyễn Văn Hoài <husstress2002@gmail.com>
2025-09-14 15:21:05 +0700

    Add Player Controller

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Rendering;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    public InputAction MoveAction;
+    Rigidbody2D rigidbody2d;
+    public int maxHealth = 5;
+    int currentHealth = 1;
+    public float speed = 3.0f;
+    Vector2 move;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        MoveAction.Enable();
+        rigidbody2d = GetComponent<Rigidbody2D>();
+        //currentHealth = maxHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        move = MoveAction.ReadValue<Vector2>();
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 position = (Vector2)rigidbody2d.position + move * speed * Time.fixedDeltaTime;
+        rigidbody2d.MovePosition(position);
+    }
+
+    public void ChangeHealth(int amount)
+    {
+        currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+        Debug.Log(currentHealth + "/" + maxHealth);
+    }
+
+}
